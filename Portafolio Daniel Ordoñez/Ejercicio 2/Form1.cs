56ca787b05cee55e82cc2332d099283a3ee8416f
Daniel-Coreas <danilittle.android@gmail.com>
2020-02-12 13:34:38 -0600

    Ejercicio 2

---
+++ Portafolio Daniel Ordoñez/Ejercicio 2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Convert.ToString(Convert.ToInt32(textBox1.Text), 2);
+            textBox3.Text = Convert.ToString(Convert.ToInt32(textBox1.Text), 8);
+            textBox4.Text = Convert.ToString(Convert.ToInt32(textBox1.Text), 16);
+        }
+
+        private void btnLimp_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
