a72b0fee804c62d38e4719fb87166074b84c65d7
Daniel-Coreas <danilittle.android@gmail.com>
2020-02-15 20:59:12 -0600

    ejercicio 4

---
+++ Portafolio Daniel Ordoñez/Practica 1/ejercicio 4.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Practica_1
+{
+    partial class ejercicio_4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnNueva = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtIva = new System.Windows.Forms.TextBox();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.txtCant = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.cboLibros = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(243, 263);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 29;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnNueva
+            // 
+            this.btnNueva.Location = new System.Drawing.Point(127, 263);
+            this.btnNueva.Name = "btnNueva";
+            this.btnNueva.Size = new System.Drawing.Size(75, 23);
+            this.btnNueva.TabIndex = 28;
+            this.btnNueva.Text = "Nueva";
+            this.btnNueva.UseVisualStyleBackColor = true;
+            this.btnNueva.Click += new System.EventHandler(this.btnNueva_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(31, 263);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 27;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(113, 216);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 26;
+            // 
+            // txtIva
+            // 
+            this.txtIva.Location = new System.Drawing.Point(113, 187);
+            this.txtIva.Name = "txtIva";
+            this.txtIva.ReadOnly = true;
+            this.txtIva.Size = new System.Drawing.Size(100, 20);
+            this.txtIva.TabIndex = 25;
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(113, 154);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.ReadOnly = true;
+            this.txtSubtotal.Size = new System.Drawing.Size(100, 20);
+            this.txtSubtotal.TabIndex = 24;
+            // 
+            // txtCant
+            // 
+            this.txtCant.Location = new System.Drawing.Point(113, 91);
+            this.txtCant.Name = "txtCant";
+            this.txtCant.Size = new System.Drawing.Size(100, 20);
+            this.txtCant.TabIndex = 23;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(113, 51);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 22;
+            // 
+            // cboLibros
+            // 
+            this.cboLibros.FormattingEnabled = true;
+            this.cboLibros.Items.AddRange(new object[] {
+            "El perfume: Historia de un asesino. Patrick Süskind",
+            "100 años de soledad. Gabriel García Marquez",
+            "Divina comedia. Dante Alighieri",
+            "Jícaras tristes. Salarrué",
+            "La casa de los espíritus. Isabel Allende",
+            "Las almas muertas. Nicolai Gogol"});
+            this.cboLibros.Location = new System.Drawing.Point(113, 15);
+            this.cboLibros.Name = "cboLibros";
+            this.cboLibros.Size = new System.Drawing.Size(253, 21);
+            this.cboLibros.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Total a pagar:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Impuesto (13%)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Subtotal:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Cantidad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Precio:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Título del libro:";
+            // 
+            // ejercicio_4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 314);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnNueva);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtIva);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.txtCant);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.cboLibros);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ejercicio_4";
+            this.Text = "ejercicio_4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnNueva;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtIva;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.TextBox txtCant;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.ComboBox cboLibros;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Portafolio Daniel Ordoñez/Practica 1/ejercicio 4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_1
+{
+    public partial class ejercicio_4 : Form
+    {
+        public ejercicio_4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double totalPago, subtotal, iva;
+            subtotal = Convert.ToDouble(txtPrecio.Text) * Convert.ToDouble(txtCant.Text);
+            iva = subtotal * 0.13;
+            totalPago = subtotal + iva;
+            txtSubtotal.Text = "$ " + Convert.ToString(Convert.ToDouble(txtPrecio.Text) * Convert.ToDouble(txtCant.Text));
+            txtIva.Text = "$ " + Convert.ToString(iva);
+            txtTotal.Text = "$ " + Convert.ToString(subtotal + iva);
+        }
+
+        private void btnNueva_Click(object sender, EventArgs e)
+        {
+            txtCant.Clear();
+            txtIva.Clear();
+            txtPrecio.Clear();
+            txtSubtotal.Clear();
+            txtTotal.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
