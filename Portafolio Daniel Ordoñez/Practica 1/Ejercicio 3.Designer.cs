7b00b6a6647cbfd50aa90819af43901a39c68959
Daniel-Coreas <danilittle.android@gmail.com>
2020-02-15 20:56:30 -0600

    Ejercicio 3

---
+++ Portafolio Daniel Ordoñez/Practica 1/Ejercicio 3.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Practica_1
+{
+    partial class Ejercicio_3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimp = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtTotalInv = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPorcentaje3 = new System.Windows.Forms.TextBox();
+            this.txtPorcentaje2 = new System.Windows.Forms.TextBox();
+            this.txtPorcentaje1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPorcentaje = new System.Windows.Forms.Label();
+            this.lblInver = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(199, 198);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 31;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimp
+            // 
+            this.btnLimp.Location = new System.Drawing.Point(118, 198);
+            this.btnLimp.Name = "btnLimp";
+            this.btnLimp.Size = new System.Drawing.Size(75, 23);
+            this.btnLimp.TabIndex = 30;
+            this.btnLimp.Text = "Limpiar";
+            this.btnLimp.UseVisualStyleBackColor = true;
+            this.btnLimp.Click += new System.EventHandler(this.btnLimp_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(37, 198);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 29;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtTotalInv
+            // 
+            this.txtTotalInv.Location = new System.Drawing.Point(37, 159);
+            this.txtTotalInv.Name = "txtTotalInv";
+            this.txtTotalInv.ReadOnly = true;
+            this.txtTotalInv.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalInv.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Total Inversión";
+            // 
+            // txtPorcentaje3
+            // 
+            this.txtPorcentaje3.Location = new System.Drawing.Point(160, 105);
+            this.txtPorcentaje3.Name = "txtPorcentaje3";
+            this.txtPorcentaje3.ReadOnly = true;
+            this.txtPorcentaje3.Size = new System.Drawing.Size(100, 20);
+            this.txtPorcentaje3.TabIndex = 26;
+            // 
+            // txtPorcentaje2
+            // 
+            this.txtPorcentaje2.Location = new System.Drawing.Point(160, 74);
+            this.txtPorcentaje2.Name = "txtPorcentaje2";
+            this.txtPorcentaje2.ReadOnly = true;
+            this.txtPorcentaje2.Size = new System.Drawing.Size(100, 20);
+            this.txtPorcentaje2.TabIndex = 25;
+            // 
+            // txtPorcentaje1
+            // 
+            this.txtPorcentaje1.Location = new System.Drawing.Point(160, 45);
+            this.txtPorcentaje1.Name = "txtPorcentaje1";
+            this.txtPorcentaje1.ReadOnly = true;
+            this.txtPorcentaje1.Size = new System.Drawing.Size(100, 20);
+            this.txtPorcentaje1.TabIndex = 24;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(37, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 23;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(37, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "1:";
+            // 
+            // lblPorcentaje
+            // 
+            this.lblPorcentaje.AutoSize = true;
+            this.lblPorcentaje.Location = new System.Drawing.Point(157, 18);
+            this.lblPorcentaje.Name = "lblPorcentaje";
+            this.lblPorcentaje.Size = new System.Drawing.Size(58, 13);
+            this.lblPorcentaje.TabIndex = 17;
+            this.lblPorcentaje.Text = "Porcentaje";
+            // 
+            // lblInver
+            // 
+            this.lblInver.AutoSize = true;
+            this.lblInver.Location = new System.Drawing.Point(34, 18);
+            this.lblInver.Name = "lblInver";
+            this.lblInver.Size = new System.Drawing.Size(71, 13);
+            this.lblInver.TabIndex = 16;
+            this.lblInver.Text = "Inversionistas";
+            // 
+            // Ejercicio_3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 244);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimp);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtTotalInv);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPorcentaje3);
+            this.Controls.Add(this.txtPorcentaje2);
+            this.Controls.Add(this.txtPorcentaje1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPorcentaje);
+            this.Controls.Add(this.lblInver);
+            this.Name = "Ejercicio_3";
+            this.Text = "Ejercicio_3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimp;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtTotalInv;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPorcentaje3;
+        private System.Windows.Forms.TextBox txtPorcentaje2;
+        private System.Windows.Forms.TextBox txtPorcentaje1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPorcentaje;
+        private System.Windows.Forms.Label lblInver;
+    }
+}
---
+++ Portafolio Daniel Ordoñez/Practica 1/Ejercicio 3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_1
+{
+    public partial class Ejercicio_3 : Form
+    {
+        public Ejercicio_3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double num1, num2, num3, total;
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            num3 = Convert.ToDouble(textBox3.Text);
+            total = num1 + num2 + num3;
+            txtPorcentaje1.Text = String.Format("{0:N1}", (num1 / total) * 100) + "%";
+            txtPorcentaje2.Text = String.Format("{0:N1}", (num2 / total) * 100) + "%";
+            txtPorcentaje3.Text = String.Format("{0:N1}", (num3 / total) * 100) + "%";
+            txtTotalInv.Text = String.Format("$" + "{0:N}", total);
+        }
+
+        private void btnLimp_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            txtPorcentaje1.Clear();
+            txtPorcentaje2.Clear();
+            txtPorcentaje3.Clear();
+            txtTotalInv.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
