68b3d72e4cd534b623ba84083a699ce68135d426
Daniel-Coreas <danilittle.android@gmail.com>
2020-02-16 16:36:56 -0600

    Finalizado

--- Portafolio Daniel Ordoñez/Practica 2/ejercicio 3.Designer.cs
+++ Portafolio Daniel Ordoñez/Practica 2/ejercicio 3.Designer.cs
@@ -50,6 +50,7 @@
             this.btnSalir.TabIndex = 15;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // btnConver
             // 
--- Portafolio Daniel Ordoñez/Practica 2/ejercicio 3.cs
+++ Portafolio Daniel Ordoñez/Practica 2/ejercicio 3.cs
@@ -73,5 +73,10 @@ namespace Practica_2
                 MessageBox.Show("Seleccione las longitudes a convertir");
             }
         }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
