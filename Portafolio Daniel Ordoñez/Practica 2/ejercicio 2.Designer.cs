f7ca8c5f30429e463e43e9638193da852bbff327
Daniel-Coreas <danilittle.android@gmail.com>
2020-02-15 21:12:03 -0600

    Ejercicio 2 practica 2

---
+++ Portafolio Daniel Ordoñez/Practica 2/ejercicio 2.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Practica_2
+{
+    partial class ejercicio_2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grp1 = new System.Windows.Forms.GroupBox();
+            this.rdo20 = new System.Windows.Forms.RadioButton();
+            this.rdo15 = new System.Windows.Forms.RadioButton();
+            this.rdo10 = new System.Windows.Forms.RadioButton();
+            this.rdo5 = new System.Windows.Forms.RadioButton();
+            this.rdo0 = new System.Windows.Forms.RadioButton();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCant = new System.Windows.Forms.Label();
+            this.grp1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grp1
+            // 
+            this.grp1.Controls.Add(this.rdo20);
+            this.grp1.Controls.Add(this.rdo15);
+            this.grp1.Controls.Add(this.rdo10);
+            this.grp1.Controls.Add(this.rdo5);
+            this.grp1.Controls.Add(this.rdo0);
+            this.grp1.Location = new System.Drawing.Point(209, 12);
+            this.grp1.Name = "grp1";
+            this.grp1.Size = new System.Drawing.Size(75, 136);
+            this.grp1.TabIndex = 23;
+            this.grp1.TabStop = false;
+            this.grp1.Text = "Descuento";
+            // 
+            // rdo20
+            // 
+            this.rdo20.AutoSize = true;
+            this.rdo20.Location = new System.Drawing.Point(7, 116);
+            this.rdo20.Name = "rdo20";
+            this.rdo20.Size = new System.Drawing.Size(45, 17);
+            this.rdo20.TabIndex = 4;
+            this.rdo20.Text = "20%";
+            this.rdo20.UseVisualStyleBackColor = true;
+            // 
+            // rdo15
+            // 
+            this.rdo15.AutoSize = true;
+            this.rdo15.Location = new System.Drawing.Point(7, 93);
+            this.rdo15.Name = "rdo15";
+            this.rdo15.Size = new System.Drawing.Size(45, 17);
+            this.rdo15.TabIndex = 3;
+            this.rdo15.Text = "15%";
+            this.rdo15.UseVisualStyleBackColor = true;
+            // 
+            // rdo10
+            // 
+            this.rdo10.AutoSize = true;
+            this.rdo10.Location = new System.Drawing.Point(7, 68);
+            this.rdo10.Name = "rdo10";
+            this.rdo10.Size = new System.Drawing.Size(45, 17);
+            this.rdo10.TabIndex = 2;
+            this.rdo10.Text = "10%";
+            this.rdo10.UseVisualStyleBackColor = true;
+            // 
+            // rdo5
+            // 
+            this.rdo5.AutoSize = true;
+            this.rdo5.Location = new System.Drawing.Point(7, 44);
+            this.rdo5.Name = "rdo5";
+            this.rdo5.Size = new System.Drawing.Size(39, 17);
+            this.rdo5.TabIndex = 1;
+            this.rdo5.Text = "5%";
+            this.rdo5.UseVisualStyleBackColor = true;
+            // 
+            // rdo0
+            // 
+            this.rdo0.AutoSize = true;
+            this.rdo0.Checked = true;
+            this.rdo0.Location = new System.Drawing.Point(7, 20);
+            this.rdo0.Name = "rdo0";
+            this.rdo0.Size = new System.Drawing.Size(39, 17);
+            this.rdo0.TabIndex = 0;
+            this.rdo0.TabStop = true;
+            this.rdo0.Text = "0%";
+            this.rdo0.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(150, 175);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 22;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(69, 175);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 21;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(82, 138);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 19;
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.Location = new System.Drawing.Point(82, 112);
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.ReadOnly = true;
+            this.txtDescuento.Size = new System.Drawing.Size(100, 20);
+            this.txtDescuento.TabIndex = 18;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(17, 115);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(59, 13);
+            this.label.TabIndex = 17;
+            this.label.Text = "Descuento";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(69, 71);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 16;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(82, 41);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 15;
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(82, 12);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 14;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(39, 44);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 13;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblCant
+            // 
+            this.lblCant.AutoSize = true;
+            this.lblCant.Location = new System.Drawing.Point(17, 12);
+            this.lblCant.Name = "lblCant";
+            this.lblCant.Size = new System.Drawing.Size(49, 13);
+            this.lblCant.TabIndex = 12;
+            this.lblCant.Text = "Cantidad";
+            // 
+            // ejercicio_2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 224);
+            this.Controls.Add(this.grp1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDescuento);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblCant);
+            this.Name = "ejercicio_2";
+            this.Text = "ejercicio_2";
+            this.grp1.ResumeLayout(false);
+            this.grp1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grp1;
+        private System.Windows.Forms.RadioButton rdo20;
+        private System.Windows.Forms.RadioButton rdo15;
+        private System.Windows.Forms.RadioButton rdo10;
+        private System.Windows.Forms.RadioButton rdo5;
+        private System.Windows.Forms.RadioButton rdo0;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblCant;
+    }
+}
