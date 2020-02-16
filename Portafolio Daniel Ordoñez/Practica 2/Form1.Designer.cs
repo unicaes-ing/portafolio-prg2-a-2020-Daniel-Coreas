793be092f076ae06b6b617224d77f84b65ab7b48
Daniel-Coreas <danilittle.android@gmail.com>
2020-02-15 21:07:29 -0600

    Ejercicio 1 practica 2

---
+++ Portafolio Daniel Ordoñez/Practica 2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Practica_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEquivale = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEquivale
+            // 
+            this.lblEquivale.AutoSize = true;
+            this.lblEquivale.Location = new System.Drawing.Point(26, 66);
+            this.lblEquivale.Name = "lblEquivale";
+            this.lblEquivale.Size = new System.Drawing.Size(120, 13);
+            this.lblEquivale.TabIndex = 16;
+            this.lblEquivale.Text = "Equivale a X en romano";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(198, 57);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 15;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(198, 12);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 14;
+            this.btnConvert.Text = "Convertir";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(71, 14);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 13;
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(14, 14);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(44, 13);
+            this.lblNum.TabIndex = 12;
+            this.lblNum.Text = "Numero";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 108);
+            this.Controls.Add(this.lblEquivale);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.lblNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEquivale;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label lblNum;
+    }
+}
+
---
+++ Portafolio Daniel Ordoñez/Practica 2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = Convert.ToInt32(txtNum.Text);
+                string romano = "";
+                if (numero >= 1 && numero <= 10)
+                {
+                    if (numero == 1)
+                    {
+                        romano = "I";
+                    }
+                    else if (numero == 2)
+                    {
+                        romano = "II";
+                    }
+                    else if (numero == 3)
+                    {
+                        romano = "III";
+                    }
+                    else if (numero == 4)
+                    {
+                        romano = "IV";
+                    }
+                    else if (numero == 5)
+                    {
+                        romano = "V";
+                    }
+                    else if (numero == 6)
+                    {
+                        romano = "VI";
+                    }
+                    else if (numero == 7)
+                    {
+                        romano = "VII";
+                    }
+                    else if (numero == 8)
+                    {
+                        romano = "VIII";
+                    }
+                    else if (numero == 9)
+                    {
+                        romano = "IX";
+                    }
+                    else if (numero == 10)
+                    {
+                        romano = "X";
+                    }
+                    lblEquivale.Text = "Equivale a " + romano + " En romano";
+                }
+                else
+                {
+                    txtNum.Focus();
+                    txtNum.SelectAll();
+                    lblEquivale.Text = "Ingrese un numero entre 1 y 10";
+                }
+            }
+            catch (Exception)
+            {
+                txtNum.Focus();
+                txtNum.SelectAll();
+                lblEquivale.Text = "Error, sólo números...";
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
